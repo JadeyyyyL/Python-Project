7946571e410acfe46dd2bdb73c9786b5ae644db6
JadeyyyL <156961283+JadeyyyL@users.noreply.github.com>
2024-04-28 17:39:46 -0400

    Update search_track.py

--- search_track.py
+++ search_track.py
@@ -13,25 +13,7 @@ def search_track(track_name, artist_name, album_name=None, year=None, genre=None
     url = "https://api.spotify.com/v1/search"
     headers = get_header(token)
     query = f"track:{track_name} artist:{artist_name}"
-    if artist_name:
-        query += f" artist:{artist_name}"
-    if album_name:
-        query += f" album:{album_name}"
-    if year:
-        query += f" year:{year}"
-    if genre:
-        query += f" genre:{genre}"
-    if market:
-        query += f" market:{market}"
-        
-    params = {"q": query, "type": "track"}
-    response_data = get(url, headers=headers, params=params)
-    data = response_data.json()
-    track_ids = [item['id'] for item in data['tracks']['items']]
-    # print(track_ids[0])
-    # pprint.pprint(data)
-    # return track_ids[0]
-    print(data)
+ 
 
 # search_track("Greedy", "Tate McRae", album_name="THINK LATER")
 search_track("Standing Next to You", "Jungkook")
