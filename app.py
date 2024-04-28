c0d77b7d0d1651de0edc66dee02fc496bb578cdd
ycao5 <ycao5@babson.edu>
2024-04-28 16:40:31 -0400

    update

--- app.py
+++ app.py
@@ -10,15 +10,34 @@ def index():
 # Handling form submission
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Get user input from the form
-    user_input = request.form['user_input']
-    # Redirect to the next page with the user input as a URL parameter
-    return redirect(url_for('results', user_input=user_input))
+    try:
+        if request.method == "POST":
+            # Get user input from the form
+            user_input = request.form['user_input']
+            # Redirect to the next page with the user input as a URL parameter
+            return render_template("results.html", user_input = user_input)
+
+    except Exception as e:
+        return render_template("error_page.html", error_message=str(e))
+
+    return render_template("index.html")
 
 # Next page
 @app.route('/results/<user_input>')
 def generate_song(user_input):
-    return f'<h1></h1>'
+    recommendation = recommended_song()
+    return render_template("results.html", user_input=user_input, recommendation = recommendation)
+
+
+#Error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error_page.html", error_message="Page not found."), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("error_page.html", error_message="Internal server error."), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
