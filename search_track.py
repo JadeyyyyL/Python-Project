b23ceeaa3bcda6eb79df0fa2043282852a3fc79d
ycao5 <ycao5@babson.edu>
2024-04-30 22:52:30 -0400

    Update search_track.py

--- search_track.py
+++ search_track.py
@@ -2,6 +2,12 @@ import spotify_token
 import pprint 
 from requests import get
 import requests
+import spotify
+
+playlist = "Today's Top Hits"
+playlist_id = spotify.search_playlist_id(playlist)
+print(playlist_id)
+song_data = spotify.search_track_by_playlist(playlist_id)
 
 def get_header(token):
     """
@@ -9,79 +15,6 @@ def get_header(token):
     """
     return {"Authorization": "Bearer " + token}
 
-
-def search_track(track_name, artist_name, album_name=None, year=None, genre=None, market=None):
-    token = spotify_token.get_token()
-    url = "https://api.spotify.com/v1/search"
-    headers = get_header(token)
-    query = f"track:{track_name} artist:{artist_name}"
-    params = {"q": query, "type": "track", "limit": 1 }
-    response = get(url, headers=headers, params=params)
-    data = response.json()
-    tracks = data.get('tracks', {}).get('items', [])
-    if tracks:
-        track_info = {
-            "name": tracks[0]["name"],
-            "artist": tracks[0]["artists"][0]["name"],
-            "album": tracks[0]["album"]["name"],
-            "preview_url": tracks[0]["preview_url"],
-            "spotify_url": tracks[0]["external_urls"]["spotify"]
-        }
-        return track_info
-    else:
-        return "Track not found. Please check the track name and artist name."
-    
-# search_track("Greedy", "Tate McRae", album_name="THINK LATER")
-#print(search_track("Standing Next to You", "Jung Kook"))
-
-def search_playlist_id(playlist_name):
-    """Returns the Spotify ID of a playlist."""
-    token = spotify_token.get_token()
-    url = "https://api.spotify.com/v1/search"
-    headers = get_header(token)
-    params = {"q": playlist_name, "type": "playlist"}
-
-    response_data = get(url, headers=headers, params=params)
-    data = response_data.json()
-    data = data["playlists"]["items"][0]
-    # pprint.pprint(data)
-    return data["id"]
-
-top_hits_playlist_id = search_playlist_id("Today's Top Hits")
-
-print("Spotify ID for Top Hits playlist:", top_hits_playlist_id)
-    
-def get_playlist_tracks(playlist_id):
-    token = spotify_token.get_token()
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    headers = get_header(token)
-    params = {"limit": 100}
-    tracks = []
-
-    
-    response = get(url, headers=headers, params=params)
-    data = response.json()
-    items = data["items"]
-    for item in items:
-        track = item['track']
-        track_info = {
-            'id': track['id'],
-            'name': track['name'],
-            'artist': track['artists'][0]['name']
-        }
-        tracks.append(track_info)
-
-    if data['next']:
-        url = data['next']
-
-    return tracks
-
-# Get tracks from Spotify's "Top Hits" playlist
-top_hits_tracks = get_playlist_tracks(top_hits_playlist_id)
-
-for idx, track in enumerate(top_hits_tracks, start=1):
-   print(f"{idx}. {track['name']} - {track['artist']}")
-   
 def search_audio_features(track_id):
     token = spotify_token.get_token()
     url = f"https://api.spotify.com/v1/audio-features/{track_id}"
@@ -93,8 +26,10 @@ def search_audio_features(track_id):
     }
     return track_features
 
-test1 = search_audio_features(top_hits_tracks[0]["id"])
-# print(test1)
+audio_features = []
+for row in song_data:
+    valence = search_audio_features(row["id"])
+    audio_features.append(valence)
 
 def get_top_hits_features(top_hits_tracks):
     top_hits_features = []  
@@ -121,3 +56,80 @@ def get_top_hits_features(top_hits_tracks):
 
 test2 = get_top_hits_features(top_hits_tracks)
 print(test2[0])
+
+
+
+
+
+# def search_track(track_name, artist_name, album_name=None, year=None, genre=None, market=None):
+#     token = spotify_token.get_token()
+#     url = "https://api.spotify.com/v1/search"
+#     headers = get_header(token)
+#     query = f"track:{track_name} artist:{artist_name}"
+#     params = {"q": query, "type": "track", "limit": 1 }
+#     response = get(url, headers=headers, params=params)
+#     data = response.json()
+#     tracks = data.get('tracks', {}).get('items', [])
+#     if tracks:
+#         track_info = {
+#             "name": tracks[0]["name"],
+#             "artist": tracks[0]["artists"][0]["name"],
+#             "album": tracks[0]["album"]["name"],
+#             "preview_url": tracks[0]["preview_url"],
+#             "spotify_url": tracks[0]["external_urls"]["spotify"]
+#         }
+#         return track_info
+#     else:
+#         return "Track not found. Please check the track name and artist name."
+    
+# # search_track("Greedy", "Tate McRae", album_name="THINK LATER")
+# #print(search_track("Standing Next to You", "Jung Kook"))
+
+# def search_playlist_id(playlist_name):
+#     """Returns the Spotify ID of a playlist."""
+#     token = spotify_token.get_token()
+#     url = "https://api.spotify.com/v1/search"
+#     headers = get_header(token)
+#     params = {"q": playlist_name, "type": "playlist"}
+
+#     response_data = get(url, headers=headers, params=params)
+#     data = response_data.json()
+#     data = data["playlists"]["items"][0]
+#     # pprint.pprint(data)
+#     return data["id"]
+
+# top_hits_playlist_id = search_playlist_id("Today's Top Hits")
+
+# print("Spotify ID for Top Hits playlist:", top_hits_playlist_id)
+    
+# def get_playlist_tracks(playlist_id):
+#     token = spotify_token.get_token()
+#     url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+#     headers = get_header(token)
+#     params = {"limit": 100}
+#     tracks = []
+
+    
+#     response = get(url, headers=headers, params=params)
+#     data = response.json()
+#     items = data["items"]
+#     for item in items:
+#         track = item['track']
+#         track_info = {
+#             'id': track['id'],
+#             'name': track['name'],
+#             'artist': track['artists'][0]['name']
+#         }
+#         tracks.append(track_info)
+
+#     if data['next']:
+#         url = data['next']
+
+#     return tracks
+
+# Get tracks from Spotify's "Top Hits" playlist
+# top_hits_tracks = get_playlist_tracks(top_hits_playlist_id)
+
+# for idx, track in enumerate(top_hits_tracks, start=1):
+#    print(f"{idx}. {track['name']} - {track['artist']}")
+   
